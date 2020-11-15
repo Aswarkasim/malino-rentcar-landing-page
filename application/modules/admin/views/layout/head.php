b504afc5499f95da34b4b41b424088ddaeca2b80
aswarkasim <aswarkasim@gmail.com>
2020-11-15 15:07:57 +0700

    aa

--- application/modules/admin/views/layout/head.php
+++ application/modules/admin/views/layout/head.php
@@ -9,7 +9,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="malinotransport" content="malino transport rental mobil">
-  <meta name="malinotransport" content="malino transport rental mobil">
+  <meta name="malinotransport" content="malino transport rental mobil mobil">
 
   <title><?= 'Admin | ' . $konfigurasi->nama_perusahaan;
           if (isset($title)) {
